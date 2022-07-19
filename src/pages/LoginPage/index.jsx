f2f19da31d48a7756e9f55a2e3333b20af099dc4
Pepo <MateusLeoSilva666@gmail.com>
2022-07-19 18:38:11 -0300

    Third Commit

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css"
 
+
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submit');
+    }
+
     return (
         <div id="login">
-            <h1 className='title'>Login do sistema</h1>
-            <form className="form">
+            <h1 className='title'> Login do sistema</h1>
+            <form className="form" onSubmit={handleSubmit}>
                 <div className="field">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder='Insira seu email'/>
+                    <input type="email" name="email" id="email" value={email} onChange={(event) => (event.target.value)} placeholder='Insira seu email'/>
                 </div>
                 <div className="field">
                     <label htmlFor="password">Senha</label>
-                    <input type="password" name="password" id="password" placeholder='Insira sua senha'/>
+                    <input type="password" name="password" id="password" value={password} onChange={(event) => (event.target.value)} placeholder='Insira sua senha'/>
                 </div>
                 <div className="actions">
                     <button type="submit">Entrar</button>
